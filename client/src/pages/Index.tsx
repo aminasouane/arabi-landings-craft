191a18f3a8a9b548326d755dc790d473603a4ab2
aminasouane <80628605+aminasouane@users.noreply.github.com>
2025-10-25 19:54:21 +0200

    Update Index.tsx

--- client/src/pages/Index.tsx
+++ client/src/pages/Index.tsx
@@ -2,14 +2,12 @@ import TalkheeselyHero from "@/components/TalkheeselyHero";
 import TalkheeselyFeatures from "@/components/TalkheeselyFeatures";
 import TalkheeselyForm from "@/components/TalkheeselyForm";
 import TalkheeselyFooter from "@/components/TalkheeselyFooter";
-import ComingSoonDialog from "@/components/ComingSoonDialog";
 const Index = () => {
   return (
     <div className="min-h-screen bg-background" dir="rtl">
       <TalkheeselyHero />
       <TalkheeselyFeatures />
       <TalkheeselyForm />
-    <ComingSoonDialog open={true} onOpenChange={() => {}} />
       <TalkheeselyFooter />
     </div>
   );
