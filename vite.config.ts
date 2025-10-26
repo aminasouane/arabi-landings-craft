940da966a8386d768d027082a8da65cd6302155a
aminasouane <80628605+aminasouane@users.noreply.github.com>
2025-10-26 09:48:39 +0100

    Update vite.config.ts
    تعديل الملف ليصبح أسهل للنشر على vercel

--- vite.config.ts
+++ vite.config.ts
@@ -1,34 +1,24 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import path from 'path';
-import { fileURLToPath } from 'url';
-import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+import path from "path";
 
 export default defineConfig({
-  base: '/arabi-landings-craft/',
-  plugins: [
-    react(),
-    runtimeErrorOverlay(),
-  ],
+  plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "client/src"),
+      "@": path.resolve(__dirname, "src"),
       "@shared": path.resolve(__dirname, "shared"),
       "@assets": path.resolve(__dirname, "attached_assets"),
     },
   },
-  root: path.resolve(__dirname, "client"),
   build: {
-    outDir: path.resolve(__dirname, "dist/public"),
+    outDir: "dist",
     emptyOutDir: true,
   },
+  base: "/", 
   server: {
-    fs: {
-      strict: true,
-      deny: ["**/.*"],
-    },
+    port: 5173, 
   },
 });
+
+      
