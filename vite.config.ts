70b9eb021b44e59b0ffe708f6558d26fd469b20d
aminasouane <80628605+aminasouane@users.noreply.github.com>
2025-10-26 09:50:06 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,55 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import path from "path";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { createRequire } from 'module';
+import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
+
+const require = createRequire(import.meta.url);
+const pkg = require('./package.json');
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default defineConfig({
-  plugins: [react()],
+  base: '/',
+  plugins: [
+    react(),
+    runtimeErrorOverlay(),
+  ],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "src"),
+      "@": path.resolve(__dirname, "client/src"),
       "@shared": path.resolve(__dirname, "shared"),
       "@assets": path.resolve(__dirname, "attached_assets"),
     },
   },
+  root: "./client",
+  publicDir: "public",
   build: {
-    outDir: "dist",
+    outDir: "../dist",
     emptyOutDir: true,
+    rollupOptions: {
+      input: path.resolve(__dirname, 'client/index.html'),
+      output: {
+        manualChunks: {
+          react: ['react', 'react-dom'],
+          radix: Object.keys(pkg.dependencies)
+            .filter(key => key.startsWith('@radix-ui/'))
+        }
+      }
+    }
   },
-  base: "/", 
   server: {
-    port: 5173, 
+    port: 3000,
+    open: '/',
+    fs: {
+      strict: true,
+      deny: ["**/.*"],
+    },
   },
 });
+
+
 
       
